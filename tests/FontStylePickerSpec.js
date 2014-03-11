b3059ae29455135f3cc8a054c3eec48c7ed924d1
mayah <mayah@wix.com>
2014-03-11 13:58:37 +0200

    Added tests for the font picker

--- tests/FontStylePickerSpec.js
+++ tests/FontStylePickerSpec.js
@@ -10,7 +10,7 @@ describe('FontStylePicker', function () {
         Wix.UI.destroy(element);
     })
 
-    it('should set the correct font class name for each item in the dropdown', function(){
+    it('should set the correct font style name for each item in the dropdown', function(){
         Wix.UI.initializePlugin(element);
         var $fontPicker = $(".font-style-picker");
         $fontPicker.find('.box-like-drop').click();
@@ -29,16 +29,49 @@ describe('FontStylePicker', function () {
             _.each($options, function(option){
                 var $option = $(option);
                 if (!$option.hasClass('current-item')){
-                    var font = $option.attr('data-value');
-                    var fontDisplayName = $option.find('.font').html();
-                    expect(fontDisplayName).toBe(givenFontName(font));
+                    var style = $option.attr('data-value');
+                    var styleDisplayName = $option.find('.font').html();
+                    expect(styleDisplayName).toBe(givenFontName(style));
                 }
 
             });
         });
     });
 
+    it('should set the correct font class name for each item in the dropdown', function(){
+        Wix.UI.initializePlugin(element);
+        var $fontPicker = $(".font-style-picker");
+        var $popup = $fontPicker.find("[wix-ctrl='Popup']");
+        expect($popup.length).toBe(1);
+        var $style = $popup.find("[wix-ctrl='Dropdown']");
+        expect($style.length).toBe(1);
+        var $options = $style.find('.option');
+
+        _.each($options, function(option){
+            var $option = $(option);
+            if (!$option.hasClass('current-item')){
+                var style = $option.attr('data-value');
+                var font = Wix.Styles.getStyleFontByReference(style);
+                var fontEl = $option.find('.font');
+                var fontFamily = fontEl.css('font-family');
+                expect(fontFamily).toBe('arial');
+            }
+
+        });
+    });
+
     it('should handle "Custom" per spec', function(){
+        Wix.UI.initializePlugin(element);
+        var $fontPicker = $(".font-style-picker");
+        var $popup = $fontPicker.find("[wix-ctrl='Popup']");
+        expect($popup.length).toBe(1);
+        var $style = $popup.find("[wix-ctrl='Dropdown']");
+        expect($style.length).toBe(1);
+
+        var customEl = $style.find('.custom');
+        expect(customEl).toBeDefined();
+        var styleDisplayName = customEl.find('.font').html();
+        expect(styleDisplayName).toBe("Custom");
 
     });
 
