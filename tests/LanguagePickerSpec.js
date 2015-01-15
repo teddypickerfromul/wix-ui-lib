f771cb61199ce4bd970d42a6c6f50ddf63440451
Lior Shefer <lior.shefer@wix.com>
2015-01-15 10:24:50 +0200

    #cleanup

--- tests/LanguagePickerSpec.js
+++ tests/LanguagePickerSpec.js
@@ -4,7 +4,7 @@ describe('LanguagePicker', function () {
     var element;
     beforeEach(function () {
         this.addMatchers({
-            toBeWixed: function() {
+            toBeWixed: function () {
                 var $option = this.actual.find('.selected .option');
                 var $globe = this.actual.find('.selected .option span.globe');
                 var $options = this.actual.find('.options .option');
@@ -12,63 +12,63 @@ describe('LanguagePicker', function () {
             }
         });
     });
-    beforeEach(function(){
+    beforeEach(function () {
         element = $('<div wix-model="languagePicker" wix-ctrl="LanguagePicker"></div>').appendTo('body')[0];
     });
 
-    afterEach(function(){
+    afterEach(function () {
         Wix.UI.destroy(element);
     });
 
-    it('should apply wix markup to given wix-ctrl', function(){
+    it('should apply wix markup to given wix-ctrl', function () {
         Wix.UI.initializePlugin(element);
         var $languagePicker = $(".uilib-languagePicker");
         expect($languagePicker).toBeWixed();
     });
 
     it('should add supported languages and values to language picker', function () {
-      var $languagePicker, $options, $elem;
-      var langs = givenLanguagesWeSupport();
-      var values = getSupportedValues();
-      
-      Wix.UI.initializePlugin(element);
-      $languagePicker = $(".uilib-languagePicker");
-      $options = $languagePicker.find('.options .option');
+        var $languagePicker, $options, $elem;
+        var langs = givenLanguagesWeSupport();
+        var values = getSupportedValues();
 
-      _.each($options, function (elem) {
-        $elem = $(elem);
-        expect(_.indexOf(langs, $elem.text())).toBeGreaterThan(-1);
-        expect(_.indexOf(values, $elem.attr('data-value'))).toBeGreaterThan(-1);
-      });
+        Wix.UI.initializePlugin(element);
+        $languagePicker = $(".uilib-languagePicker");
+        $options = $languagePicker.find('.options .option');
+
+        _.each($options, function (elem) {
+            $elem = $(elem);
+            expect(_.indexOf(langs, $elem.text())).toBeGreaterThan(-1);
+            expect(_.indexOf(values, $elem.attr('data-value'))).toBeGreaterThan(-1);
+        });
     });
 
     describe('Default Options', function () {
-        beforeEach(function(){
+        beforeEach(function () {
             Wix.UI.initializePlugin(element);
         });
-        it('should set selected language to En', function(){
+        it('should set selected language to En', function () {
             var $languagePicker = $(".uilib-languagePicker");
-            var $selected =  $languagePicker.find('.selected .option');
+            var $selected = $languagePicker.find('.selected .option');
             expect($selected.text()).toBe('En');
         });
 
-        it('should add a globe span to selected option', function(){
+        it('should add a globe span to selected option', function () {
             var $languagePicker = $(".uilib-languagePicker");
-            var $selected =  $languagePicker.find('.selected .option');
+            var $selected = $languagePicker.find('.selected .option');
             expect($selected.find("span.globe").length).toEqual(1);
         });
     });
 
-    it('should set selected value from the option data-value', function(){
+    it('should set selected value from the option data-value', function () {
         Wix.UI.initializePlugin(element);
         var $languagePicker = $(".uilib-languagePicker");
         var $deutsch = $languagePicker.find('.options .option:nth-child(2)');
         $deutsch.click();
-        var $selected =  $languagePicker.find('.selected .option');
+        var $selected = $languagePicker.find('.selected .option');
         expect($selected.text()).toBe($deutsch.attr('data-value'));
     });
 
-    it('should trigger ChangeEvent when option is selected', function(){
+    it('should trigger ChangeEvent when option is selected', function () {
         Wix.UI.initializePlugin(element);
         var $languagePicker = $(".uilib-languagePicker");
         var $es = $languagePicker.find('.options .option:nth-child(3)');
@@ -78,10 +78,10 @@ describe('LanguagePicker', function () {
     });
 
     function givenLanguagesWeSupport() {
-       return ['English', 'Deutsch', 'Español', 'Français', 'Italiano', 'Polski', 'Português', 'Русский', '日本語', '한국어', 'Türkçe', 'Nederlands'];
+        return ['English', 'Deutsch', 'Español', 'Français', 'Italiano', 'Polski', 'Português', 'Русский', '日本語', '한국어', 'Türkçe', 'Nederlands'];
     }
-    
+
     function getSupportedValues() {
-      return ['En', 'De', 'Es', 'Fr', 'It', 'Pl', 'Pt', 'Ru', 'Ja', 'Ko', 'Tr', 'Nl'];
+        return ['En', 'De', 'Es', 'Fr', 'It', 'Pl', 'Pt', 'Ru', 'Ja', 'Ko', 'Tr', 'Nl'];
     }
 });
