3f12f419d8bd16b6987c69fc9d2db463658f8329
Adiel <adiela@wix.com>
2015-02-05 16:23:41 +0200

    changing release task to use gitHub access-token instead of username && password which impossible when two-factor authentication is active

--- tasks/release.js
+++ tasks/release.js
@@ -13,28 +13,27 @@ module.exports = function(grunt) {
         child.on('exit', this.async());
     }
 
-    function upload(username, password, release, assetName){
-        _.bind(execCommon, this, 'curl', ['-u', username + ':' + password,
+    function upload(accessToken, release, assetName){
+        _.bind(execCommon, this, 'curl', ['-u', accessToken + ':x-oauth-basic',
             '-H', 'Accept: application/vnd.github.manifold-preview', '-H', 'Content-Type: application/zip',
             '--data-binary', '@archive.zip',
             'https://uploads.github.com/repos/wix/wix-ui-lib/releases/' + release +'/assets?name=' + assetName + '.zip'])();
     }
 
     grunt.registerTask('release-create-upload', 'create a new release on github.com', function() {
-        var username = grunt.option('username');
-        var password = grunt.option('password');
+        var accessToken = grunt.option('access-token');
         var tag_name = grunt.option('tag');
         var name = grunt.option('name');
         var body = grunt.option('body');
-        if(username && password && tag_name && name && body){
-            _.bind(execCommon, this, 'curl', ['-u', username + ':' + password,
+        if(accessToken && tag_name && name && body){
+            _.bind(execCommon, this, 'curl', ['-u', accessToken + ':x-oauth-basic',
                 '-d', '{"tag_name":"' + tag_name + '","target_commitish": "master","name": "' + name + '","body":"' + body + '","draft": false,"prerelease": false}',
                 'https://api.github.com/repos/wix/wix-ui-lib/releases'], function(data){
-                _.bind(upload, this, username, password, data.id, data.name)();
+                _.bind(upload, this, accessToken, data.id, data.name)();
             }.bind(this))();
         } else {
             grunt.warn('Missing require argument(s). Try something like: '+
-                'grunt release --username=EMAIL --password=PASSWORD --tag=v1.0.0 --name=v1.0.0 --body=\'Description of the release\'')
+                'grunt release --access-token --tag=v1.0.0 --name=v1.0.0 --body=\'Description of the release\'')
         }
     });
 };
